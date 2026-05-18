c5136f633833e744fbebac2a23b4793c9ff17ad5
Savva <sav.van.ivan3@mail.ru>
2026-05-18 13:20:24 +0300

    Initial commit

---
+++ src/main/java/com/project/collaborationplatform/collaborationplatformauthenticationservice/CollaborationPlatformAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.collaborationplatform.collaborationplatformauthenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborationPlatformAuthenticationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollaborationPlatformAuthenticationServiceApplication.class, args);
+    }
+
+}
